d6aabd8f677171059cac35c5e696064e92ef7dd3
Trover <74741960+Troversones@users.noreply.github.com>
2021-10-14 08:45:45 +0200

    Update fasz.ts

--- fasz.ts
+++ fasz.ts
@@ -1,7 +1,14 @@
-class Ember {
+interface IEmber {
+  hajszin: string;
+  koszones() : void;
+}
+class Ember implements IEmber{
   szemekSzama: number;
   nev: string;
-
+  hajszin: string;
+  koszones():void{
+    console.log(`Szia, ${this.nev} vagyok.`);
+  }
   constructor(nev: string) {
     this.szemekSzama = 2;
     this.nev = nev;
@@ -11,13 +18,21 @@ class Boxolo extends Ember {
   
   constructor(nev: string) {
     super(nev);
+    this.koszones();
   }
 }
 var boxos = new Boxolo("Géza");
 console.log(boxos);
-class Faszomataphpba extends Boxolo {
-  Félszemű: string;
-  constructor(Félszemű: string) {
-    super(Félszemű);
+class Lacika extends Boxolo {
+  Sárgalapos: boolean;
+  constructor() {
+    super('Lacika');
+    this.Sárgalapos = Math.random() > 0.5;
+  }
+  koszones():void{
+    console.log(`Csá tesám, van egy százasod?`);
   }
 }
+var lacika = new Lacika();
+console.log(lacika);
+var boxolo = new Boxolo("Géza");
